4610ce2cbbee8d94a0174854523a6b4d8808439a
Alvaro8-has <83841582+Alvaro8-has@users.noreply.github.com>
2024-01-23 21:52:39 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,4 @@
+<div class="page">
+    <h2>About Us</h2>
+    <p>Alvaro Dewa berdiri pada tahun 2023 sebagai penyedia playlist di Indonesia yang aman, cepat, dan andal. Kami merupakan penyedia playlist IPTV.</p>
+</div>
